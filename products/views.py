ad04c3024c949dd308ea0bea18947902464f03a0
annabrynkina <an.brynkina@gmail.com>
2023-05-09 13:24:43 +0300

    Added products page

--- products/views.py
+++ products/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# created a controller to show the page
 def index(request):
-    return render(request)
+    return render(request, 'products/index.html')
+
+def products(request):
+    return render(request, 'products/products.html')
