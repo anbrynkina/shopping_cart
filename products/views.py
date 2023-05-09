d799777835a09a55667c0cafff1543c3c63cc14e
annabrynkina <an.brynkina@gmail.com>
2023-05-09 16:03:27 +0300

    Added django template inheritance for base.html, products.html, index.html. Added webpage title for products and homepage (index.html)

--- products/views.py
+++ products/views.py
@@ -4,16 +4,31 @@ from django.shortcuts import render
 
 # created a controller to show the page
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store Homepage'
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Products Catalogue'
+    }
+    return render(request, 'products/products.html', context)
 
 # to test how dynamic content works, creating test function
 def test_context(request):
     context = {
-        'title': 'Store',
+        'title': 'store',
         'header': 'Welcome to the store!',
         'username': 'Anna',
+        'products': [
+            {'name': 'adidas Original Black Monogram Hoodies', 'price': 80},
+            {'name': 'The North Face blue jacket', 'price': 100},
+            {'name': 'ASOS DESIGN oversize sports top in brown', 'price': 59.9},
+        ],
+        # 'promotion': True,
+        'products_of_promotion': [
+            {'name': 'Nike Black Heritage Backpack', 'price': 69},
+        ]
     }
     return render(request, 'products/test_context.html', context)
