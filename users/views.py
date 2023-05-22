89fbc2777aabcfca15d420003bfc1b2173baee1f
annabrynkina <an.brynkina@gmail.com>
2023-05-22 14:27:10 +0300

    Modified user/views.py login request

--- users/views.py
+++ users/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from users.forms import UserLoginForm
+from django.contrib import auth
+
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'users/login.html')
+    #
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            # if the user is in our system and if he is active, then:
+            if user and user.is_active:
+                # then authorize him
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    # get request:
+    form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
 
 def register(request):
     return render(request, 'users/register.html')
-
